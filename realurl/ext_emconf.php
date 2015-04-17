193a30fc181346f2576532f41df4010551091488
Sergio Catalá <sergio.catala@e-net.info>
2015-04-17 13:48:00 +0200

    Updates realurl extension to 1.13.3.

--- realurl/ext_emconf.php
+++ realurl/ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "realurl".
  *
- * Auto generated 11-04-2014 14:15
+ * Auto generated 17-04-2015 13:01
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -14,30 +14,21 @@ $EM_CONF[$_EXTKEY] = array (
 	'title' => 'RealURL: speaking paths for TYPO3',
 	'description' => 'Creates nice looking URLs for TYPO3 pages: converts http://example.com/index.phpid=12345&L=2 to http://example.com/path/to/your/page/. Please, ask for free support in TYPO3 mailing lists or contact the maintainer for paid support.',
 	'category' => 'fe',
-	'shy' => 0,
-	'version' => '1.12.8',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
+	'version' => '1.13.3',
 	'state' => 'stable',
-	'uploadfolder' => 0,
+	'uploadfolder' => true,
 	'createDirs' => '',
-	'modify_tables' => 'pages,sys_domain,pages_language_overlay,sys_template',
-	'clearcacheonload' => 1,
-	'lockType' => '',
+	'clearcacheonload' => false,
 	'author' => 'Dmitry Dulepov',
 	'author_email' => 'dmitry.dulepov@gmail.com',
 	'author_company' => '',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
 	'constraints' => 
 	array (
 		'depends' => 
 		array (
 			'php' => '5.3.2-5.999.999',
-			'typo3' => '4.5.0-6.2.999',
+			'typo3' => '4.5.40-7.1.999',
+			'lowlevel' => '1.0.0-100.0.0',
 		),
 		'conflicts' => 
 		array (
@@ -50,4 +41,3 @@ $EM_CONF[$_EXTKEY] = array (
 	),
 );
 
-?>
